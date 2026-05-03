b41074aad8ea54e9c9ba1356e3244b59c2674bfb
jackrice13 <jackrice13@gmail.com>
2026-05-03 11:35:41 -0400

    admin portal changes.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,73 @@
 from django.contrib import admin
 from .models import UserProfile, VulnerabilityStatus
 
-admin.site.register(UserProfile)
-admin.site.register(VulnerabilityStatus)
-from django.contrib import admin
 
-# Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+    # columns shown in the list view
+    list_display = ['user', 'get_email', 'get_vendor_list', 'get_vendor_count']
+
+    # searchable fields
+    search_fields = ['user__username', 'user__email']
+
+    # filter sidebar
+    list_filter = ['vendors']
+
+    # allows editing vendors directly on the list page
+    filter_horizontal = ['vendors']
+
+    def get_email(self, obj):
+        # pulls email from the related User object
+        return obj.user.email
+    get_email.short_description = 'Email'
+
+    def get_vendor_count(self, obj):
+        # counts how many vendors this user tracks
+        return obj.vendors.count()
+    get_vendor_count.short_description = 'Vendor Count'
+
+    def get_vendor_list(self, obj):
+        # returns a comma separated list of vendor names
+        return ', '.join([v.name for v in obj.vendors.all()])
+    get_vendor_list.short_description = 'Tracked Vendors'
+
+
+class VulnerabilityStatusAdmin(admin.ModelAdmin):
+    # columns shown in the list view
+    list_display = [
+        'get_username',
+        'get_cve_id',
+        'status',
+        'get_severity',
+        'updated_at'
+    ]
+
+    # filter sidebar - filter by status or severity
+    list_filter = ['status', 'vulnerability__severity']
+
+    # search by username or CVE ID
+    search_fields = [
+        'profile__user__username',
+        'vulnerability__cve_id'
+    ]
+
+    # default sort - most recently updated first
+    ordering = ['-updated_at']
+
+    def get_username(self, obj):
+        # pulls username from the related UserProfile → User chain
+        return obj.profile.user.username
+    get_username.short_description = 'Username'
+
+    def get_cve_id(self, obj):
+        # pulls CVE ID from the related Vulnerability
+        return obj.vulnerability.cve_id
+    get_cve_id.short_description = 'CVE ID'
+
+    def get_severity(self, obj):
+        # pulls severity from the related Vulnerability
+        return obj.vulnerability.severity
+    get_severity.short_description = 'Severity'
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(VulnerabilityStatus, VulnerabilityStatusAdmin)
--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,60 @@
 from django.contrib import admin
 from .models import Vendor, Software, Vulnerability
 
-admin.site.register(Vendor)
-admin.site.register(Software)
-admin.site.register(Vulnerability)
+
+class SoftwareInline(admin.TabularInline):
+    # shows software entries directly inside the vendor admin page
+    model = Software
+    extra = 0  # don't show empty extra rows
+    fields = ['name', 'version', 'end_of_life_date']
+
+
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'get_software_count']
+    search_fields = ['name', 'slug']
+
+    # shows software inline when editing a vendor
+    inlines = [SoftwareInline]
+
+    def get_software_count(self, obj):
+        return obj.software_set.count()
+    get_software_count.short_description = 'Software Count'
+
+
+class SoftwareAdmin(admin.ModelAdmin):
+    list_display = ['name', 'vendor', 'version', 'end_of_life_date']
+    list_filter = ['vendor']
+    search_fields = ['name', 'vendor__name']
+    ordering = ['vendor__name', 'name']
+
+
+class VulnerabilityAdmin(admin.ModelAdmin):
+    list_display = [
+        'cve_id',
+        'severity',
+        'cvss_score',
+        'in_cisa_kev',
+        'published_date',
+        'get_software_count'
+    ]
+
+    # filter sidebar
+    list_filter = ['severity', 'in_cisa_kev']
+
+    # search by CVE ID or description
+    search_fields = ['cve_id', 'description']
+
+    # default sort - highest CVSS first
+    ordering = ['-cvss_score']
+
+    # allows editing software links directly on the edit page
+    filter_horizontal = ['software']
+
+    def get_software_count(self, obj):
+        return obj.software.count()
+    get_software_count.short_description = 'Affected Software'
+
+
+admin.site.register(Vendor, VendorAdmin)
+admin.site.register(Software, SoftwareAdmin)
+admin.site.register(Vulnerability, VulnerabilityAdmin)
